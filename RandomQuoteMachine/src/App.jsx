6394da0f39558b5e178940eab2045e5544d23ca1
Dalton <c_c_llanos@hotmail.com>
2023-10-01 04:44:25 -0300

    feat: Agregar función para obtener citas aleatorias
    Se ha implementado una función llamada 'getQuote()' en el componente. Esta función se encarga de obtener citas aleatorias, aunque en este ejemplo se utiliza una cita ficticia como marcador de posición. Al hacer clic en el botón 'Get Random Quote', se invoca esta función y se actualiza el estado del componente con la cita recuperada.
    
    # Cambios propuestos:
    - Implementada la función 'getQuote()' para obtener citas aleatorias.
    - El estado del componente se actualiza con la cita ficticia como marcador de posición.

--- RandomQuoteMachine/src/App.jsx
+++ RandomQuoteMachine/src/App.jsx
@@ -11,8 +11,32 @@ class MyApp extends Component {
       text: ''
     }
    };
+
+   getQuote() {
+    // You can make an API request here and update the state with the retrieved quote
+    // For this example, I'll use a placeholder quote
+    const placeholderQuote = {
+      author: 'John Doe',
+      text: 'This is a random quote.'
+    };
+
+    this.setState({ quote: placeholderQuote });
+  }
+
+
   render() { 
-    return (  );
+    return ( 
+      <div>
+        <div>
+          <h1>Random Quote Machine</h1>
+          <button onClick={() => this.getQuote()}>Get Random Quote</button>
+        </div>
+        <div>
+          <p>{this.state.quote.text}</p>
+          <p>- {this.state.quote.author}</p>
+        </div>
+      </div>
+     );
   }
 }
  
